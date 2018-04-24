938457e8f293f403d17dbbc4e56563456fc1dd98
neokdev <neokdev@gmail.com>
2018-04-24 21:45:53 +0200

    User Entity added

---
+++ src/Entity/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var UuidInterface
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @var array
+     */
+    private $roles;
+
+    /**
+     * @var bool
+     */
+    private $enabled;
+
+    /**
+     * @var bool
+     */
+    private $userNonExpired;
+
+    /**
+     * @var bool
+     */
+    private $credentialsNonExpired;
+
+    /**
+     * @var bool
+     */
+    private $userNonLocked;
+
+    /**
+     * @return UuidInterface
+     */
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+}
