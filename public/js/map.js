cd464da2b00037550608c28da299122ca3ad2af9
jeremiehvt <jeremiehvt@gmail.com>
2018-05-23 16:33:32 +0200

    files(add files): add files for the observe formtype
    add ObserveType ObserveTypeHandler ObserveDTO

---
+++ public/js/map.js
@@ -0,0 +1,29 @@
+// map
+
+let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/neokiller113/cjgpehvma00992sqmhyxveaa6/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibmVva2lsbGVyMTEzIiwiYSI6ImNqZ2h2c2xxMzBsbm0ycWsxemJ6YnNpZXEifQ.PiGeZsNjVNyo1G80Y1KD4Q'
+}).addTo(mymap);
+
+// autocomplete
+
+
+httpRequest = new XMLHttpRequest();
+httpRequest.open(
+    'POST',
+
+)
+httpRequest.send();
+
+let elem = document.querySelector('.autocomplete');
+let instance = M.Autocomplete.init(elem, {
+    data: {
+        "Hiboux": null,
+        "Moineau": null,
+        "Chapeau": null,
+    }
+});
