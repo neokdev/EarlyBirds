c3914ff3a3a46ecfaa5305a738e12c84c79a9f54
neokdev <neokdev@gmail.com>
2018-06-08 18:44:09 +0200

    fix(db) TimestampableEntity is now working very well fix(db) Fixed "regne" field in taxref feat(bundles) Added KnpTimeBundle for displaying dates on views fix(fixture) Dates is now until 1 years to now, Latitude and Longitude in France fix(db) Observe entity refactoring fix(menu) Icons fixes TODO : "Mon compte and the icon" feat(release) { Many my maps added to profile account Images resizing Json ajax call Taxref info Handling }

--- src/DataFixtures/Observe.php
+++ src/DataFixtures/Observe.php
@@ -5,14 +5,26 @@ namespace App\DataFixtures;
 use App\Domain\Models\Observe as Observes;
 use App\Domain\Models\TaxRef;
 use App\Domain\Models\User;
+use App\Domain\Repository\TaxRefRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class Observe extends AbstractBaseFixture
 {
     private const TEST_USER = 'neokdev@gmail.com';
-    private const TIMEZONE  = 'Europe/Paris';
     private $user;
-    private $ref;
+    /**
+     * @var TaxRefRepository
+     */
+    private $taxRefRepository;
+
+    /**
+     * Observe constructor.
+     * @param TaxRefRepository $taxRefRepository
+     */
+    public function __construct(TaxRefRepository $taxRefRepository)
+    {
+        $this->taxRefRepository = $taxRefRepository;
+    }
 
     /**
      * @param ObjectManager $manager
@@ -22,15 +34,21 @@ class Observe extends AbstractBaseFixture
     protected function loadData(ObjectManager $manager)
     {
         $this->user = $manager->getRepository(User::class)->findOneBy(['email' => self::TEST_USER]);
-        $this->ref  = $manager->getRepository(TaxRef::class)->findOneBy(['id' => rand(0, 3983)]);
 
+        $taxref = null;
         $this->createMany(Observes::class, 10, function (Observes $observe, $count) {
-            $observe->setAuthor($this->user)
-                ->setRef($this->ref)
-                ->setDescription($this->faker->sentence)
-                ->setLatitude($this->faker->latitude)
-                ->setLongitude($this->faker->longitude)
-                ->setImg($this->faker->imageUrl(1920, 1080, 'animals', true, 'observe', false));
+            /** @var TaxRef $taxref */
+            while (!isset($taxref) || null === $taxref->getNomVern()) {
+                $taxref = $this->taxRefRepository->findOneBy(['id' => rand(0, 3983)]);
+            }
+
+            $observe->setAuthor($this->user);
+            $observe->setRef($taxref);
+            $observe->setLatitude($this->faker->latitude(42, 51));
+            $observe->setDescription($this->faker->text);
+            $observe->setLongitude($this->faker->longitude(-5, 8));
+            $observe->setImg($this->faker->imageUrl(1920, 1080, 'animals', true, 'observe', false));
+            $observe->setCreatedAt($this->faker->dateTimeBetween('-1 year', 'now'));
         });
 
         $manager->flush();
