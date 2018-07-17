12b82ef94a5b5de1f2c558272735c5ab1a887a21
havart jérémie <jeremiehvt@gmail.com>
2018-07-17 21:56:04 +0200

    feat(element) : add template discover birds
    design dicsover birds responsive template layout

---
+++ src/UI/Responder/Interfaces/DiscoverBirdsResponderInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: havartjeremie
+ * Date: 17/07/2018
+ * Time: 19:00
+ */
+
+namespace App\UI\Responder\Interfaces;
+
+
+interface DiscoverBirdsResponderInterface
+{
+    public function __invoke();
+}
