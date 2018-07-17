b7b64099b0c2cbf8c31f6c9b91896f869ebbe0b6
havart jérémie <jeremiehvt@gmail.com>
2018-07-17 23:19:44 +0200

    feat(element) : add discover nao template
    design responvie discover nao template

---
+++ src/UI/Action/Interfaces/DiscoverNaoActionInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: havartjeremie
+ * Date: 17/07/2018
+ * Time: 21:59
+ */
+
+namespace App\UI\Action\Interfaces;
+
+
+interface DiscoverNaoActionInterface
+{
+    public function __invoke();
+}
---
+++ src/UI/Responder/Interfaces/DiscoverNaoResponderInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: havartjeremie
+ * Date: 17/07/2018
+ * Time: 22:03
+ */
+
+namespace App\UI\Responder\Interfaces;
+
+
+interface DiscoverNaoResponderInterface
+{
+    public function __invoke();
+}
