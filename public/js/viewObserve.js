474ea6c7e7576ba43d1b9727d18c2bd5a51d65d1
neokdev <neokdev@gmail.com>
2018-07-20 11:52:04 +0200

    feat(release) Renaming heart.js feat(release) Addind viewObserve.js feat(release) Adding upgradeUser command

--- public/js/viewObserve.js
+++ public/js/viewObserve.js
@@ -26,6 +26,44 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 $(document).ready(function() {
+
+    $('.js-like-observe').on('click', function (e) {
+        e.preventDefault();
+
+        let $link = $(e.currentTarget);
+
+        $link.find('.material-icons').hide();
+        $link.find('#ajaxLoading').show();
+
+        $.ajax({
+            method: 'POST',
+            url: $link.attr('href')
+        }).done(function (data) {
+            $link.parent().find('.js-like-observe-count').html(data.hearts);
+            $link.find('.material-icons').show();
+            $link.find('#ajaxLoading').hide();
+
+            switch ($link.find('.material-icons').html()) {
+                case "favorite_border":
+                    $link.find('.material-icons').html('favorite');
+                    break;
+                case "favorite":
+                    $link.find('.material-icons').html('favorite_border');
+                    break;
+            }
+        });
+    });
+
+    let url = $('#heart').data('url');
+    $.getJSON(url, function (data) {
+        $.each(data, function (key, val) {
+            let id = val;
+            if ($('#heart').data('id') === id) {
+                $('#heart').html("favorite")
+            }
+        })
+    });
+
     $('.js-taxrefrang').each(function () {
         $(this).html('<b>Rang : </b>' + rang($(this).data('rang')));
     });
