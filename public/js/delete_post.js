bf97f420c3169962c08a35f98286d72a0547aea1
havart jérémie <jeremiehvt@gmail.com>
2018-07-23 21:17:24 +0200

    change(core) : modify security
    change security control in update post action and add post action

--- public/js/delete_post.js
+++ public/js/delete_post.js
@@ -3,7 +3,7 @@ $(function () {
     $('.data-delete-post').on('click', function () {
          $.ajax({
          method: "DELETE",
-         url: document.location.origin + $('.data-delete-post').attr('data-delete'),
+         url: document.location.origin + $(this).attr('data-delete'),
          timeout: 3000,
          success: function () {
              document.location.reload();
