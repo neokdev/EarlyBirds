2e0e1fd6e9d61437368f3f7fcb87017fa9445537
neokdev <neokdev@gmail.com>
2018-08-07 17:01:45 +0200

    fix(framework) Fix for pass testing

--- public/js/delete_post.js
+++ public/js/delete_post.js
@@ -1,16 +1,18 @@
-$(function () {
-    $('a.data-delete-post').on('click', function (e) {
-         e.preventDefault(this);
-         $.ajax({
-         method: "DELETE",
-         url: $(this).attr('data-delete'),
-         timeout: 3000,
-         success: function (data) {
-             M.toast({html: data});
-                 setTimeout(function () {
-                     document.location.reload();
-                 }, 2500);
-             }
-         });
+$(document).ready(function () {
+    $(document).on('click', '.data-delete-post', function (event) {
+        event.preventDefault();
+        $.ajax({
+            method: "DELETE",
+            url: $(this).attr('data-delete'),
+            timeout: 3000,
+            success: function (data) {
+                M.toast({html: data});
+                setTimeout(function () {
+                    document.location.reload();
+                }, 2500);
+            }
+        });
+
+        return false;
     });
 });
